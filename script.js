36cd12d25c03efd47fbf6ae13bb5c86a9f417d04
Rico Rose <r.andereck@gmail.com>
2023-07-11 22:53:44 -0500

    Doneish

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+       
+let pilotName = document.querySelector("input[name=pilot]").value;
+let copilotName = document.querySelector("input[name=copilot]").value;
+let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+let cargoMass = document.querySelector("input[name=cargoMass]").value;
+
+    event.preventDefault();
+
+    formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+
+});
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+
+       let planet = pickPlanet(listedPlanets);
+       let name = planet.name;
+       let diameter = planet.diameter;
+       let star = planet.star;
+       let distance = planet.dinstance;
+       let imageUrl = planet.image;
+       let moons = planet.moons;
+
+       addDestinationInfo(document, name, diameter, star, distance, imageUrl, moons);
+       console.log(planet);
+   
+    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,53 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
-}
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+       if (testInput === "") {
+          return "Empty"
+
+       } else if (isNaN(testInput) === true) {
+        return "Not a Number"
+       } else if (isNan(testInput) === false) {
+        return "Is a Number"
+       }
+    };
+
+
+function formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoLevel) {
+//Pilot-----
+   if (validateInput(pilotName) != "Empty") {
+    let pilotStatus = document.querySelector("#pilotStatus");
+    pilotStatus.innerHTML = `${pilotName} Ready`
+   }
+
+   if(validateInput(pilotName) = "Empty") {
+    alert("All fields are required.");
+   }
+
+   if (validateInput(pilot) === "Is a Number") {
+    alert ("Invalid Input for Pilot Name");
+};
+
+//Copilot-----
+   if (validateInput(copilotNamepilot) != "Empty") {
+    let pilotStatus = document.querySelector("#pilotStatus");
+    pilotStatus.innerHTML = `${pilotName} Ready`
+   }
+ 
+   if(validateInput(copilotName) = "Empty" {
+    alert("All fields are required.");
+   }
+
+   if (validateInput(copilotName) === "Is a Number") {
+    alert ("Invalid Input for Pilot Name");
+   }
+};
+
+//Fuel Level
    
+   validateInput(fuelLevel);
+   //google google "change visibility of element javascript css", "change color of element javascript css"
+   validateInput(cargoLevel);
 }
 
 async function myFetch() {
